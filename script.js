aa054e8686f11daff30ab5ddf0d51e966fe89c95
Mariana Clarice <mclaricesilva04@gmail.com>
2026-04-20 22:20:17 -0300

    feat: lightmode

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function OnInput() {
     this.style.height = "auto";
     this.style.height = (this.scrollHeight) + "px";
 }
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+
+    if (body.classList.contains('light-mode')) {
+        themeToggle.classList.remove('fa-sun');
+        themeToggle.classList.add('fa-moon');
+        themeToggle.title = "Modo escuro";
+    } else {
+        themeToggle.classList.remove('fa-moon');
+        themeToggle.classList.add('fa-sun');
+        themeToggle.title = "Modo claro";
+    }
+});
